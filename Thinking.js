47e2695820c889fbfba93664d819a7e54551fe08
Jacob Brewer <jakethebeest12@gmail.com>
2020-03-04 14:29:07 -0800

    you will continue to work on this

--- Thinking.js
+++ Thinking.js
@@ -4,16 +4,50 @@ class thinking{
 		this.happieness=10;
 		this.wellbeing=10;
 		this.satifaction=2;
-		this.thoughts="";
-		this.memory=new memory();
+		this.mem=[];
 	this.input;
-	
+	this.previnput;
+	//mem[0]=[input,[[input1,positive.length/(negitive.length+positive.length),[positive],[negitive]],[input2,percent,[positive],[negitive]],[input3...]],
 	
 	}
-	think(){
+	
+		
+	thinking(){
+
+	}	
+	
+	problemidentifire(){
 		
 		
 	}
+	problemsolving(){
+		//should think up a solotion and run it in a forcasting.
+	}
+	
+	forcasting(accuracy,y,input=this.input,input2=this.input,time=1000000000){
+	if(time=0){
+		return input;
+	}
+	let x="";
+	let prevx=x;
+	if(input!=undefined){
+	if(input==input2&&indexOfX(this.mem,input)!=-1){
+		
+	for(let j=0;j<this.mem[indexOfX(this.mem,input].length;j++){
+		if(this.mem[indexOfX(this.mem,input][1][j][1]>=accuracy){
+			x+=","+forcasting(accuracy,x,time-1,input);
+			break;
+		}
+	}
+	if(prevx==x){
+		return input;
+	}
+		
+	}
+		}
+	}
+	
+	
 	satifactionrules(){
 		
 			this.happieness-=10-this.wellbeing;
